4ad20692409ef96e15240f4dd4c90fa7593fb315
Al <chernyshov@megatec.ru>
2014-11-21 17:51:10 +0300

    First commit

---
+++ BigBuild/BigBuild.py
@@ -0,0 +1,22 @@
+import configparser
+
+def Main():
+    config = configparser.ConfigParser()
+    config.read('MW.ini')
+    
+    #вытягиваем из конфига настройки для web.config
+    connection = config['WEBCONFIG']['connection']
+    email = config['WEBCONFIG']['email']
+    smtp_server = config['WEBCONFIG']['smtp_server']
+
+    settings=connection,email,smtp_server
+    print (type(connection))
+  
+   
+Main()
+
+    
+
+
+
+
